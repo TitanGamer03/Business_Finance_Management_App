e329e2ecef76ec806d46c022663c71ad23f6ad22
TitanGamer03 <tarangpatel@gmail.com>
2025-02-14 17:02:13 +0530

    AddPage Completed.

---
+++ packages/expense_repository/lib/src/models/category.dart
@@ -0,0 +1,11 @@
+// class Category{
+//   late String categoryId;
+//   late String name;
+//   late int total_expense;
+//   late String icon;
+//   late String color;
+//
+//   Category({required this.categoryId, required this.name, required this.total_expense, required this.icon, required this.color}){
+//
+//   }
+// }
